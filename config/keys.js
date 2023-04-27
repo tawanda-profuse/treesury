17bfee56a329b12cd4ffe45019380a1cd35d0ba1
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-04-27 14:18:54 +0200

    Added all the files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// dbPassword = 'mongodb+srv://Tawanda:'+ encodeURIComponent('it%Cw76cb6t4XKe') + '@tsf-stock.gsmjj.mongodb.net/TSF_Stock?retryWrites=true&w=majority';
+dbPassword = 'mongodb://0.0.0.0:27017'
+module.exports = {
+    mongoURI: dbPassword
+};
