2906586f1504623d91352275a7b80d2ee5841610
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-04-27 16:27:09 +0200

    Changed placeholder text color, and added select dropdown and input element

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav');
+var addNew = document.getElementById("#add-new");
+var categorySelector = document.getElementById("#category-selector");
+var categoryInput = document.querySelector(".form-item select option:last-child");
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
